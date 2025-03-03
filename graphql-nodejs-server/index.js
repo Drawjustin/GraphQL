3e5bd3fa39596949f9e2912bef5cd4ea374a6e03
DrawJustin <guswhd903@naver.com>
2025-03-03 19:55:53 +0900

    ADD : Cart Resolver 구현, CONTENT : Altair 테스트 완료

--- graphql-nodejs-server/index.js
+++ graphql-nodejs-server/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
 const fs = require('fs').promises;
-
+const { mergeResolvers } = require('@graphql-tools/merge');
+const userResolver = require('./resolvers/user-resolver');
+const cartResolver = require('./resolvers/cart-resolver');
 const app = express();
 const port = 8081;
 
@@ -14,22 +16,17 @@ const schemaFiles = [
     './schemas/user.graphql',
     './schemas/cart.graphql'
   ];
-
-const typeDefs = gql`
-    type Query {
-        hello: String!
-    }
-`
-const resolvers = {
-
-}
+const resolvers = mergeResolvers([
+    userResolver,
+    cartResolver
+]);
 
 async function startServer() {
-    const schmeas = await Promise.all(
+    const schemas = await Promise.all(
         schemaFiles.map(file => fs.readFile(file, 'utf-8'))
     );
 
-    const server = new ApolloServer({ typeDefs: schmeas, resolvers});
+    const server = new ApolloServer({ typeDefs: schemas, resolvers});
     await server.start();
     server.applyMiddleware({ app });
     
